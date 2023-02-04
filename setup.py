cf707321f26b0de4ad08d0a49e2f78f54efcbb12
AJStangl <ajstangl@gmail.com>
2023-02-04 09:25:15 -0500

    init

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+	name="sexy-diffusion",
+	version="0.0.1",
+	author="AJ Stangl",
+	author_email="ajstangl@gmail.com",
+	description="A simple wrapper for collecting, transforming, and creating gpt2 models based on reddit users/subreddits",
+	license="MIT",
+	keywords="GPT2",
+	include_package_data=True,
+	url="https://example.com",
+	packages=['shared_code'],
+
+	long_description=read('README.md'),
+	classifiers=[
+		"Topic :: Utilities",
+		"License :: MIT License",
+	],
+	entry_points={
+		'console_scripts': [
+			'simple-bot-gpt2 = shared_code.cli_wrapper:cli',
+		],
+	},
+)
